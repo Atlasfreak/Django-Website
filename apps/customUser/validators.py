31efa694b51bb0bb48b4c8d161134589e281364d
Atlasfreak <60364953+Atlasfreak@users.noreply.github.com>
2020-11-21 13:24:41 +0100

    corrected name validator

--- apps/customUser/validators.py
+++ apps/customUser/validators.py
@@ -5,11 +5,11 @@ from django.utils.deconstruct import deconstructible
 from django.utils.translation import gettext_lazy as _
 
 class UnicodeFullNameValidator(validators.RegexValidator):
-    regex = r'^[a-zA-Z\s][^\n\t\d]+\Z'
+    regex = r'^([ \u00c0-\u01ffa-z\'\-])+$'
     message = _(
         'Please give your full name!'
     )
-    flags = 0
+    flags = re.IGNORECASE
 
 class UnicodeUsernameValidator(validators.RegexValidator):
     regex = r'^[\w.@+-]+\Z'

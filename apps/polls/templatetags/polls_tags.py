917bb4598db6c02258a800735dbb4f9dcc675745
Per <atlasfreak@gmx.de>
2021-01-02 14:22:30 +0100

    added better character wrap filter;

--- apps/polls/templatetags/polls_tags.py
+++ apps/polls/templatetags/polls_tags.py
@@ -1,7 +1,15 @@
+import textwrap
 from django import template
+from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
 @register.filter
 def index(indexable, i):
     return indexable[i]
+
+@register.filter
+@stringfilter
+def charwrap(value, width):
+    textwrapper = textwrap.TextWrapper(width=width,break_long_words=True,replace_whitespace=False)
+    return textwrapper.fill(value)

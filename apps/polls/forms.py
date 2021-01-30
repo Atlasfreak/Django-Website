7450ac6b122fc7bdfc0793f7d78283770ca31f65
Per <atlasfreak@gmx.de>
2021-01-30 04:59:01 +0100

    created get_QuestionTypeParamForm;
    added extra_params to AnswerForm;
    moved QuestionTypeParamCreateForm;

--- apps/polls/forms.py
+++ apps/polls/forms.py
@@ -1,9 +1,18 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.utils.html import escape
 from django.utils.module_loading import import_string
 from django.utils.translation import gettext_lazy as _
 
-from .models import Answer, Choice, Poll, Question, Submission
+from .models import (
+    Answer,
+    Choice,
+    Poll,
+    Question,
+    QuestionType,
+    QuestionTypeParam,
+    Submission,
+)
 
 
 class PollCreationForm(forms.ModelForm):
@@ -20,7 +29,47 @@ class PollCreationForm(forms.ModelForm):
         widgets = {"info_text": forms.Textarea()}
 
 
-def get_AnswerModelForm(self, question, **kwargs):
+class QuestionTypeParamCreateForm(forms.ModelForm):
+    class Meta:
+        model = QuestionTypeParam
+        fields = "__all__"
+
+    def clean(self):
+        cleaned_data = super().clean()
+        question_types = cleaned_data.get("question_type")
+        attribute = cleaned_data.get("name")
+
+        for type in question_types:
+            field = type.get_field_class()
+
+            if not hasattr(field(), attribute):
+                raise ValidationError(
+                    _("%(field)s does not have attribute: %(attribute)s."),
+                    params={"field": type, "attribute": attribute},
+                    code="invalid",
+                )
+
+
+def get_QuestionTypeParamForm(
+    q_type_param: QuestionTypeParam, prefix: str = None, replace: str = None, **kwargs
+):
+    param_dict = q_type_param.get_param_dict()
+    field_name = param_dict["name"]
+
+    Form: forms.Form = type(
+        "QuestionTypeParam_" + field_name + "_Form",
+        (forms.Form,),
+        {field_name: param_dict["field"](label=q_type_param.verbose_name)},
+    )
+    initial = {field_name: param_dict["default"]}
+    if prefix is None:
+        prefix = "__prefix__"
+    if replace is not None:
+        prefix = prefix.replace("__prefix__", replace)
+    return Form(initial=initial, prefix=prefix, **kwargs)
+
+
+def get_AnswerModelForm(self, question: Question, **kwargs):
     class AnswerForm(forms.ModelForm):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
@@ -28,6 +77,8 @@ def get_AnswerModelForm(self, question, **kwargs):
             field_class = question.type.get_field_with_widget()
 
             params = {"label": escape(question.text), "required": question.required}
+            if question.extra_params:
+                params.update(question.extra_params)
             if hasattr(field_class, "queryset"):
                 params["queryset"] = question.choices.all()
             field = field_class(**params)

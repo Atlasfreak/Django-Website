053235547446786a327845314ff22935632dffcb
Per <atlasfreak@gmx.de>
2021-01-30 04:55:28 +0100

    created QuestionTypeManager; added get_ids_to_params method;

--- apps/polls/managers.py
+++ apps/polls/managers.py
@@ -6,3 +6,12 @@ class PollManager(models.Manager):
     def are_published(self):
         now = timezone.now()
         return self.get_queryset().filter(start_date__lte=now, end_date__gt=now)
+
+
+class QuestionTypeManager(models.Manager):
+    def get_ids_to_params(self):
+        queryset = self.get_queryset()
+        dict = {}
+        for query in queryset:
+            dict.update(query.get_id_to_param())
+        return dict

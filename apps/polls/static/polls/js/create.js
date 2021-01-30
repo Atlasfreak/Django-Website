89d95d263cd9aaf9fa9e74d34f2627c7baa08d53
Per <atlasfreak@gmx.de>
2021-01-30 05:07:57 +0100

    moved most javascript to create.js; added forms for entering extra_params; added most javascript from inline HTML; added function to show extra_param forms; improved function to change wether Options are Available;

--- apps/polls/static/polls/js/create.js
+++ apps/polls/static/polls/js/create.js
@@ -31,7 +31,7 @@ function removeForm(btn) {
     if (form_total > min_forms) {
         button.parentsUntil(data_target).remove();
         let children = btn_parent.children();
-        console.log(children)
+
         for (let i = 0, len = children.length; i < len; i++) {
             let regex = new RegExp(form_id + '-\\d+-', 'g');
             let replacement = form_id + '-' + i + '-';
@@ -43,20 +43,77 @@ function removeForm(btn) {
     }
 }
 
+function changeAvailableParams(target, options, val) {
+    const regex = new RegExp('[a-z]*-\\d+-', 'gi');
+    const param_regex = new RegExp('__prefix__-', 'g')
+    const ids_to_params = JSON.parse($('#param_ids_to_forms').text());
+    let prefix = target.attr('name').match(regex);
+    let params_div = $('#question_type_params');
+
+    let before_options = options.prevUntil('.card-body').filter(function (index) {
+        let array = Object.values(ids_to_params).flat(1);
+        if (array.includes($(this).attr('class'))) {
+            return true;
+        }
+        return false;
+    });
+
+    if (val in ids_to_params) {
+        let params = ids_to_params[val];
+        params.forEach(param => {
+            let input = params_div.find('input[name=__prefix__-' + param + ']');
+            let copy_div = input.parents('.form-group');
+
+            if (!options.prevAll('div.' + param).length) {
+                let copied = copy_div.clone().prependTo(options.parents('.card-body')).wrap('<div class=' + param + '></div>');
+                if (params.indexOf(param) == 0) {
+                    copied.append('<hr>');
+                }
+                copied[0].outerHTML = copied[0].outerHTML.replaceAll(param_regex, prefix);
+            }
+
+        });
+    } else if (before_options) {
+        $(before_options).remove();
+    }
+}
+
 function changeAvailableOptions(origin, parent, options_id, values, message) {
+    const class_name = 'options_unavailable'
+    const html_tag = 'h5'
     let target = $(origin);
     let val = parseInt(target.val());
     let options = target.parents(parent).find(options_id);
 
+    changeAvailableParams(target, options, val)
     if (values.includes(val)) {
         options.hide(0);
-        if (!options.prev('div.options_unavailable').length) {
-            options.before('<div class="options_unavailable">' + message + '</div>')
+        if (!options.prevAll(html_tag + '.' + class_name).length) {
+            options.before('<' + html_tag + ' class="' + class_name + '">' + message + '</' + html_tag + '>')
         }
     } else {
-        if (options.prev('div.options_unavailable')) {
-            options.prev('div.options_unavailable').remove();
+        if (options.prevAll(html_tag + '.' + class_name)) {
+            options.prevAll(html_tag + '.' + class_name).remove();
         }
         options.show(0);
     }
 }
+
+$(document).ready(function () {
+    const options_deactivated = JSON.parse($('#options_deactivated').text())
+    $('.custom-select').each(function () {
+        changeAvailableOptions(this, '.card', '#options', options_deactivated, 'Dieser Fragetyp lässt keine Auswahlmöglichkeiten zu.');
+    });
+
+    $('form').on('click', '.add_form', function () {
+        addForm(this);
+    });
+
+    $('form').on('click', '.remove_form', function () {
+        removeForm(this);
+    });
+
+    $('form').on('change', '.custom-select', function () {
+        changeAvailableOptions(this, '.card', '#options', options_deactivated, 'Dieser Fragetyp lässt keine Auswahlmöglichkeiten zu.')
+    });
+});

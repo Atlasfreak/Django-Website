9014eb6309f4bc571b4e0801b8b5483a0d1e7b92
Per <atlasfreak@gmx.de>
2021-02-04 04:34:47 +0100

    added password reset functionality;

--- website/urls.py
+++ website/urls.py
@@ -38,6 +38,48 @@ urlpatterns = [
     ),
     path("register/", user_views.register, name="register"),
     path("polls/", include("apps.polls.urls")),
+    path(
+        "password/",
+        include(
+            [
+                path(
+                    "reset/",
+                    include(
+                        [
+                            path(
+                                "",
+                                auth_views.PasswordResetView.as_view(
+                                    template_name="userManagement/password_reset/reset.html"
+                                ),
+                                name="password_reset",
+                            ),
+                            path(
+                                "done/",
+                                auth_views.PasswordResetDoneView.as_view(
+                                    template_name="userManagement/password_reset/done.html"
+                                ),
+                                name="password_reset_done",
+                            ),
+                            path(
+                                "confirm/<uidb64>/<token>/",
+                                auth_views.PasswordResetConfirmView.as_view(
+                                    template_name="userManagement/password_reset/confirm.html"
+                                ),
+                                name="password_reset_confirm",
+                            ),
+                            path(
+                                "complete/",
+                                auth_views.PasswordResetCompleteView.as_view(
+                                    template_name="userManagement/password_reset/complete.html"
+                                ),
+                                name="password_reset_complete",
+                            ),
+                        ]
+                    ),
+                ),
+            ]
+        ),
+    ),
 ]
 
 if settings.DEBUG:

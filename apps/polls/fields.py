4ec0128fbf2295f0d5bba518621a657ab67d332f
Per <atlasfreak@gmx.de>
2021-02-24 02:21:13 +0100

    added CustomModelChoiceField;

--- apps/polls/fields.py
+++ apps/polls/fields.py
@@ -1,4 +1,4 @@
-from django.forms.models import ModelChoiceField, ModelMultipleChoiceField
+from django.forms.models import ModelChoiceField, ModelForm, ModelMultipleChoiceField
 
 from .validators import MaxNumberValidator, MinNumberValidator
 
@@ -24,3 +24,28 @@ class ModelLimitMultipleChoiceField(ModelMultipleChoiceField):
             self.validators.append(MaxNumberValidator(max_number))
         if min_number is not None:
             self.validators.append(MinNumberValidator(min_number))
+
+
+class CustomModelChoiceField(ModelChoiceField):
+    """
+    ModelChoiceField that converts strings via a dictionary to valid ids.
+    Needed for a related question of :model:`polls.Choice`.
+
+    Example:
+        >>> from polls.models import Question
+        >>> string_to_id = {"question-0": 1}
+        >>> field = CustomModelChoiceField(Question.objects.get(id=1), string_to_id=string_to_id)
+        >>> field.clean()
+    """
+
+    def __init__(self, queryset, *, string_to_id=None, **kwargs):
+        if string_to_id is None:
+            self.string_to_id = {}
+        else:
+            self.string_to_id = string_to_id
+        super().__init__(queryset, **kwargs)
+
+    def to_python(self, value):
+        if self.string_to_id:
+            value = self.string_to_id[value]
+        return super().to_python(value)

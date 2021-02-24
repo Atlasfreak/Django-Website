157bc2c9d970466e7e12c0d497815a3de367cda9
Per <atlasfreak@gmx.de>
2021-02-24 02:25:30 +0100

    added get_field_name filter; added docstrings;

--- apps/polls/templatetags/polls_tags.py
+++ apps/polls/templatetags/polls_tags.py
@@ -1,15 +1,35 @@
 import textwrap
+
 from django import template
 from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
+
 @register.filter
 def index(indexable, i):
+    """
+    Return the value of a indexable at the specified index.
+    """
     return indexable[i]
+
 
 @register.filter
 @stringfilter
 def charwrap(value, width):
-    textwrapper = textwrap.TextWrapper(width=width,break_long_words=True,replace_whitespace=False)
+    textwrapper = textwrap.TextWrapper(
+        width=width, break_long_words=True, replace_whitespace=False
+    )
     return textwrapper.fill(value)
+
+
+@register.filter
+@stringfilter
+def get_field_name(value: str):
+    """
+    Returns the name of the form field
+    by splitting the value at every "-" character
+    and returning the last part of that value.
+    """
+    splitted = value.split("-")
+    return splitted[-1]

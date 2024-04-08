0209cf4943ba3a0e3b5bb0d4c0d85243995205d2
Per Göttlicher <atlasfreak@gmx.de>
2024-04-08 04:04:58 +0200

    add darmstadt_termine as app and to urls

--- website/urls.py
+++ website/urls.py
@@ -13,8 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from apps.main import views as main_views
-from apps.userManagement import views as user_views
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -22,6 +21,9 @@ from django.contrib.auth import views as auth_views
 from django.contrib.flatpages import views as flat_views
 from django.urls import include, path
 from django.views.generic.base import TemplateView
+
+from apps.main import views as main_views
+from apps.userManagement import views as user_views
 
 urlpatterns = [
     path("admin/doc/", include("django.contrib.admindocs.urls")),
@@ -88,6 +90,7 @@ urlpatterns = [
     ),
     path("contact/", flat_views.flatpage, {"url": "/contact/"}, name="contact"),
     path("legal/", flat_views.flatpage, {"url": "/contact/"}, name="legal"),
+    path("darmstadt/", include("darmstadt_termine.urls")),
 ]
 
 handler404 = "website.error_handlers.handler404"

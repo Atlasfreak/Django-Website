bfdac2b36d08ef0583a01ff33082528ff9971bc3
Per <atlasfreak@gmx.de>
2024-04-13 06:18:06 +0200

    add localization middleware update dependencies

--- website/settings.py
+++ website/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     "django.middleware.gzip.GZipMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
